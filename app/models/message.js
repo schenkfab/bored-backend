d5f07dbe29335c90fedbd9b9b427caff41a0b0df
JioxoFab <fschenker@windowslive.com>
2016-05-14 20:29:44 +0200

    feat: add messages

---
+++ app/models/message.js
@@ -0,0 +1,14 @@
+/*
+	A message object for mongoDb containing a sender, a receiver, a time and a boolean indicating
+	wheter or not the message has been read.
+*/
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('Message', new Schema({
+	sender: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+	receiver: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+	sentOn: Schema.Types.Date,
+	message: String,
+	isRead: Boolean
+}));
---
+++ routes/messages.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/messages')
+
+
+
+module.exports = router;
