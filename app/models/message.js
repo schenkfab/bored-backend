f150163e767125e90068f314ba5af0d52522679c
JioxoFab <fschenker@windowslive.com>
2016-05-15 21:05:20 +0200

    each message only has one sender and one receiver

--- app/models/message.js
+++ app/models/message.js
@@ -6,8 +6,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = mongoose.model('Message', new Schema({
-	sender: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-	receiver: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+	sender: { type: Schema.Types.ObjectId, ref: 'User' },
+	receiver: { type: Schema.Types.ObjectId, ref: 'User' },
 	sentOn: Schema.Types.Date,
 	message: String,
 	isRead: Boolean
