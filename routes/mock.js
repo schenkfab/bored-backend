0fb821fdbe28e208439f3f5d6c1ea240d2d18c4e
JioxoFab <fschenker@windowslive.com>
2016-05-16 17:36:47 +0200

    feat: finalize mock api endpoint

--- routes/mock.js
+++ routes/mock.js
@@ -4,10 +4,11 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../app/models/user');
+var Message = require('../app/models/message');
 
 
 router.post('/mock', function(req, res) {
-  var users = [
+  const users = [
     {
       name: 'Admin',
       password: 'admin',
@@ -23,12 +24,51 @@ router.post('/mock', function(req, res) {
   User.remove({}, function(err) {
     if (err) throw err;
 
-    User.create(users, function (err, users) {
-      res.json(users);
-    });
+    // remove all messages
+    Message.remove({}, function(err) {
+      if (err) throw err;
 
+      User.create(users, function (err, users) {
+        let messages = [
+          {
+            sender: users[0]._id,
+            receiver: users[1]._id,
+            sentOn: (new Date()),
+            message: 'Hello John, how are you?',
+            isRead: 1
+          }, {
+            sender: users[0]._id,
+            receiver: users[1]._id,
+            sentOn: (new Date()),
+            message: 'Hope to see you again soon.',
+            isRead: 0
+          }, {
+            sender: users[1]._id,
+            receiver: users[0]._id,
+            sentOn: (new Date()),
+            message: 'I am good, thanks.',
+            isRead: 1
+          }, {
+            sender: users[1]._id,
+            receiver: users[0]._id,
+            sentOn: (new Date()),
+            message: 'Have a nice weekend!.',
+            isRead: 0
+          }
+        ];
+
+        Message.create(messages, function(err, messages) {
+          if (err) throw err;
+
+          res.json({
+            users, messages
+          });
+
+        });
+      });
+
+    });
   });
-  
 });
 
 module.exports = router;
