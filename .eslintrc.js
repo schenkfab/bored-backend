ca71fa7752c93bc7bf22cccd8e1eb9837c8a670d
schenkfab <schenkfab@users.noreply.github.com>
2016-05-18 12:05:56 +0200

    remove unused comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,3 @@
-/*module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    },
-    "globals": {
-    "angular": true,
-    "module": true,
-    "inject": true
-  }
-};*/
-
 module.exports = {
   "extends": "airbnb"
 }
