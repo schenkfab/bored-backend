947317a816081cbec242a458ccfbb9ca629d8359
schenkfab <schenkfab@users.noreply.github.com>
2016-05-30 19:59:25 +0200

    allow token in header

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const contactsRoutes = require('./routes/contact');
 const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,token');
 
   next();
 };
