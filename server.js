79ac86b56cae8949d2b80dfb9b31a978f75023d2
schenkfab <schenkfab@users.noreply.github.com>
2016-08-07 20:04:20 +0200

    get front end url from config file

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const contactsRoutes = require('./routes/contact');
 
 // CORS middleware
 const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', config.frontend);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type,token,x-access-token');
 
